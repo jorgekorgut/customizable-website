7ab8eefe8e16d88d5f0a0cabb9573f6d3d269459
jorgekorgut <jorgekorgut@hotmail.com>
2023-02-28 22:33:36 +0100

    Quick fix

--- frontend/src/components/card/card_wide_image_title_text/card_wide_image_title_text.js
+++ frontend/src/components/card/card_wide_image_title_text/card_wide_image_title_text.js
@@ -2,13 +2,18 @@ import "./card_wide_image_title_text.css";
 export function CardWideImageTilteText(props) {
     return (<div className="card_wide">
         <div className="image_holder_wide">
-            <img src={props.src} alt={props.alt}/>
+            <img src={props.src} alt={props.alt} />
         </div>
         <div className="content_holder">
             <h1>{props.title}</h1>
-            <div className="description">{props.description.split('\n').map((value,index) => {
-                return <p key={index}>{value}</p>
-            })}</div>
+            <div className="description">
+                {
+                    props.description !== null &&
+                    props.description.split('\n').map((value, index) => {
+                        return <p key={index}>{value}</p>
+                    })
+                }
+            </div>
             <div className="content_footer"><p>{props.footer}</p></div>
         </div>
     </div>);
